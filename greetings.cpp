92b667b61a21c8e6c549d3909e3bad039c3c4923
Mykhailo Halchenko <114739882+MykhailoHalchenko@users.noreply.github.com>
2022-10-03 22:11:26 +0300

    Add files via upload

---
+++ greetings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string text, name;
+    int num;
+
+    cout << "Назови своё имя: " << endl;
+    cin >> name;
+    cout << name << "С днюхой!!" << endl;
+    
+}
